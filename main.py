ff3af8bc569ea46b2ccf2640fa092dc2d95d4180
Alexander <sasha-kurlov@yandex.ru>
2013-04-17 22:30:49 +0600

    get length of hidden image

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import pickle
+import struct
+
 file = open('data/Золотое утро_s.bmp', 'rb').read()
+bits = "".join([str(i % 2) for i in file[54:54 + 32]])
+len_bytes = bytes([int(s, 2) for s in [bits[0:8], bits[8:16], bits[16:24], bits[24:32]]])
+#struct.unpack("<I", len_bytes)[0]
